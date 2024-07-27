f62e029ee7d8f445277a9cc5654191ceded38d6f
jet_black_ninja <pramodkmr958@gmail.com>
2024-07-28 02:41:58 +0530

    main file done

--- curl_function.h
+++ curl_function.h
@@ -2,11 +2,11 @@
 #define CURL_FUNCTION_H
 
 #include <string> 
-#include <jsoncpp/json/json.h>
+#include <include/json/json.h>
 
 using namespace std;
 
-string getPrice(const string& ticker_symbol, cont string & api);
+string getPrice(const string& ticker_symbol, const string & api);
 Json::Value getStockQuote(const string & tickerSymbol, const string & api);
 
 #endif // CURL_FUNCTION_H
